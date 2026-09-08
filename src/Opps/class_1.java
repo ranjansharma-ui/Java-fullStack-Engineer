06c2cfd469b71f359289a5a2ac4916ef52ab19a8
Ranjan Kumar <ranjankumar4278907@gmail.com>
2026-09-08 22:55:58 +0530

    hi

---
+++ src/Opps/class_1.java
@@ -0,0 +1,33 @@
+package Opps;
+
+class Student {
+    String name;
+    int age;
+    int rollNumber;
+    String college;
+
+    void markAttadance() {
+        System.out.println("Attendance marked by " + name);
+    }
+
+    void print() {
+        System.out.println(name + " " + age + " " + rollNumber + " " + college);
+    }
+}
+
+public class class_1 {
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.name = "Ranjan";
+        s.age = 20;
+        s.rollNumber = 115;
+        s.college = "NIET";
+
+        s.markAttadance();
+        s.print();
+    }
+}
+
+// Java is almost object oriented programming language
+
--- src/Ranjan.java
+++
@@ -1,5 +0,0 @@
-public class Ranjan {
-    public static void main(String[] args) {
-        System.out.println("Welcome to start new jounery");
-    }
-}
